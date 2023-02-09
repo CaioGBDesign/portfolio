5fe1dd3d455d5a3ec417cc8d0545fc7f2822384a
CaioGBDesign <67526531+CaioGBDesign@users.noreply.github.com>
2023-02-09 11:23:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,137 +1,12 @@
-function projetos() {
-    if (document.getElementById('Figma').checked) {
-        document.getElementById('projeto-Figma').style.display = 'block';
-        document.getElementById('projeto-UX').style.display = 'none';
-        document.getElementById('projeto-UI').style.display = 'none';
-        document.getElementById('projeto-Blender').style.display = 'none';
-    }
-    else if(document.getElementById('UX').checked) {
-        document.getElementById('projeto-Figma').style.display = 'none';
-        document.getElementById('projeto-UX').style.display = 'block';
-        document.getElementById('projeto-UI').style.display = 'none';
-        document.getElementById('projeto-Blender').style.display = 'none';
-   }
-   else if(document.getElementById('UI').checked) {
-       document.getElementById('projeto-Figma').style.display = 'none';
-       document.getElementById('projeto-UX').style.display = 'none';
-       document.getElementById('projeto-UI').style.display = 'block';
-       document.getElementById('projeto-Blender').style.display = 'none';
-  }
-  else if(document.getElementById('Blender').checked) {
-      document.getElementById('projeto-Figma').style.display = 'none';
-      document.getElementById('projeto-UX').style.display = 'none';
-      document.getElementById('projeto-UI').style.display = 'none';
-      document.getElementById('projeto-Blender').style.display = 'block';
-  }
-}
-
-function projetos2() {
-    if (document.getElementById('AI').checked) {
-        document.getElementById('projeto-AI').style.display = 'block';
-        document.getElementById('projeto-PS').style.display = 'none';
-        document.getElementById('projeto-AE').style.display = 'none';
-        document.getElementById('projeto-PR').style.display = 'none';
-    }
-    else if(document.getElementById('PS').checked) {
-        document.getElementById('projeto-AI').style.display = 'none';
-        document.getElementById('projeto-PS').style.display = 'block';
-        document.getElementById('projeto-AE').style.display = 'none';
-        document.getElementById('projeto-PR').style.display = 'none';
-   }
-   else if(document.getElementById('AE').checked) {
-    document.getElementById('projeto-AI').style.display = 'none';
-    document.getElementById('projeto-PS').style.display = 'none';
-    document.getElementById('projeto-AE').style.display = 'block';
-    document.getElementById('projeto-PR').style.display = 'none';
-  }
-  else if(document.getElementById('PR').checked) {
-    document.getElementById('projeto-AI').style.display = 'none';
-    document.getElementById('projeto-PS').style.display = 'none';
-    document.getElementById('projeto-AE').style.display = 'none';
-    document.getElementById('projeto-PR').style.display = 'block';
-  }
-}
-
-function projetos3() {
-    if (document.getElementById('HTML').checked) {
-        document.getElementById('projeto-HTML').style.display = 'block';
-        document.getElementById('projeto-ACSS').style.display = 'none';
-        document.getElementById('projeto-CSS').style.display = 'none';
-        document.getElementById('projeto-JS').style.display = 'none';
-    }
-    else if(document.getElementById('ACSS').checked) {
-        document.getElementById('projeto-HTML').style.display = 'none';
-        document.getElementById('projeto-ACSS').style.display = 'block';
-        document.getElementById('projeto-CSS').style.display = 'none';
-        document.getElementById('projeto-JS').style.display = 'none';
-   }
-   else if(document.getElementById('CSS').checked) {
-    document.getElementById('projeto-HTML').style.display = 'none';
-    document.getElementById('projeto-ACSS').style.display = 'none';
-    document.getElementById('projeto-CSS').style.display = 'block';
-    document.getElementById('projeto-JS').style.display = 'none';
-  }
-  else if(document.getElementById('JS').checked) {
-    document.getElementById('projeto-HTML').style.display = 'none';
-    document.getElementById('projeto-ACSS').style.display = 'none';
-    document.getElementById('projeto-CSS').style.display = 'none';
-    document.getElementById('projeto-JS').style.display = 'block';
-  }
-}
-
-function regua() {
-    if (document.getElementById('regua1').checked) {
-        document.getElementById('marcador-regua1').style.opacity = '1';
-        document.getElementById('marcador-regua2').style.opacity = '0';
-        document.getElementById('marcador-regua3').style.opacity = '0';
-        document.getElementById('marcador-regua4').style.opacity = '0';
-        document.getElementById('marcador-regua5').style.opacity = '0';
-    }
-    else if(document.getElementById('regua2').checked) {
-        document.getElementById('marcador-regua1').style.opacity = '0';
-        document.getElementById('marcador-regua2').style.opacity = '1';
-        document.getElementById('marcador-regua3').style.opacity = '0';
-        document.getElementById('marcador-regua4').style.opacity = '0';
-        document.getElementById('marcador-regua5').style.opacity = '0';
-   }
-   else if(document.getElementById('regua3').checked) {
-       document.getElementById('marcador-regua1').style.opacity = '0';
-       document.getElementById('marcador-regua2').style.opacity = '0';
-       document.getElementById('marcador-regua3').style.opacity = '1';
-       document.getElementById('marcador-regua4').style.opacity = '0';
-       document.getElementById('marcador-regua5').style.opacity = '0';
-  }
-  else if(document.getElementById('regua4').checked) {
-      document.getElementById('marcador-regua1').style.opacity = '0';
-      document.getElementById('marcador-regua2').style.opacity = '0';
-      document.getElementById('marcador-regua3').style.opacity = '0';
-      document.getElementById('marcador-regua4').style.opacity = '1';
-      document.getElementById('marcador-regua5').style.opacity = '0';
- }
- else if(document.getElementById('regua5').checked) {
-     document.getElementById('marcador-regua1').style.opacity = '0';
-     document.getElementById('marcador-regua2').style.opacity = '0';
-     document.getElementById('marcador-regua3').style.opacity = '0';
-     document.getElementById('marcador-regua4').style.opacity = '0';
-     document.getElementById('marcador-regua5').style.opacity = '1';
-}
-}
-
-//voltar ao topo
-let BTNDownload = document.getElementById("BaixarCV");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-    BTNDownload.style.opacity = "1";
-  } else {
-    BTNDownload.style.opacity = "0";
-  }
-}
 
 //PopUp qualidoc
-function qualidoc()
+function qualidocA()
+{
+  var qualidoc = document.getElementById("PopupQualidoc");
+  qualidoc.style.display="flex";
+}
+
+function qualidocB()
 {
   var qualidoc = document.getElementById("PopupQualidoc");
   qualidoc.style.display="flex";
@@ -193,4 +68,220 @@ function fecharOnvip()
 {
   var fecharOnvip = document.getElementById("PopupOnvip");
   fecharOnvip.style.display="none";
+}
+
+// Direção de arte
+
+function CopiarA()
+{
+  navigator.clipboard.writeText('#DF5297').then(() => 
+  { 
+    console.log('codigo copiado')
+  }); 
+}
+
+function FundoRosa()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#DF5297";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoVerdeEscuro()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#98BA7D";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoVerdeClaro()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#91BEB9";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoRoxo()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#7D80C7";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoAzulClaro()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#A5D3E0";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoAzulEscuro()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#51A4F4";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoLaranjaClaro()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#FFC16E";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoLaranja()
+{
+  var Laranja = document.getElementById("adobe");
+  Laranja.style.background="#FF6A18";
+
+  var Laranja = document.getElementById("titulo");
+  Laranja.style.background="none";
+  Laranja.style.color="#ffffff";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ffffff";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(-10px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
+}
+
+function FundoOriginal()
+{
+  var original = document.getElementById("adobe");
+  original.style.background="#21132C";
+
+  var original = document.getElementById("titulo");
+  original.style.color="#1ABBC9";
+
+  var paragrafoDarte = document.getElementById("paragrafo-darte");
+  paragrafoDarte.style.color="#ECD4FF";
+
+  var original = document.getElementById("original");
+  original.style.transform="translateY(0px)";
+
+  const boxes = document.querySelectorAll('.box-pantene');
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = '1';
+  });
 }
