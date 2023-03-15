d0eddc76b785f7f43a8c35918d6b5ad38c77dc08
CaioGBDesign <67526531+CaioGBDesign@users.noreply.github.com>
2023-03-15 01:36:07 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,78 @@
+let quiz_rules = [
+    "Você só tem 20 segundos para responder cada questão.",
+    "Após marcar cada resposta não poderá desmarcar.",
+    "Você receberá 1 ponto a cada resposta correta."
+];
+
+
+let quiz_questions = [
+    // question 1
+    {
+        // question
+        question: "Do que se trata a heurística do afeto de Kahneman?",
+
+        // options
+        options : [
+            "Escolhas baseadas em experiências anteriores.",
+            "Decisões tomadas mais rapidamente por meio do julgamento emocional.",
+            "Quando a quantidade interfere na decisão do usuário.",
+        ],
+
+        // correct answer
+        answer: 2,
+    },
+    
+    {
+        // write yout question here
+        question: "O princípio do prazer, que diz que buscamos instintivamente o prazer e não a dor, foi pensado por quem?",
+    
+        options: [
+            "Nielsen",
+            "Freud",
+            "Kahneman",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "O que é a heurística de Bandwagon?",
+    
+        options: [
+            "Viés cognitivo da prova social.",
+            "Viés da impulsividade.",
+            "Viés da ambiguidade",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Quantidade de informações significa qualidade?",
+    
+        options: [
+            "Sim, é importante que o usuário tenha em mãos tudo o que puder.",
+            "Não, informação não interfere na experiência.",
+            "Não. O segredo é saber distribuir as informações, pois quanto maior a quantidade, menos o usuário vai ler.",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "A psicologia é importante para o UX?",
+    
+        options: [
+            "Ok, não exagera.",
+            "Nem sempre, muitas vezes, o básico é o único necessário.",
+            "Sim, o comportamento do usuário é um estudo primordial para obtenção de resultados.",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    }
+    
+];
