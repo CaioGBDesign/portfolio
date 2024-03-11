820f98c3a6d0495d7ae0703b34f55652cbc2286a
CaioGBDesign <caio.gbdesign@gmail.com>
2024-03-11 01:35:46 -0300

    alterações menu e mais

--- remove-mobile-itens.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var elementosARemover = document.getElementsByClassName("elemento-a-remover");
-    for (var i = 0; i < elementosARemover.length; i++) {
-        if (window.innerWidth <= 992) { 
-            elementosARemover[i].parentNode.removeChild(elementosARemover[i]);
-        }
-    }
-};
